0008e869d76eb3b501407c4cd8d0568c3f5bbee5
Kevin Sun <sunzhipeng8@gmail.com>
2025-09-27 14:37:24 +0800

    update

--- src/agents/message_processor_text.py
+++ src/agents/message_processor_text.py
@@ -416,6 +416,7 @@ class MessageProcessorText:
                         result["func"] = result["func"] if result["func"] in ["sit", "stand"] else "stand"
                         result["name"] = item.item_name
                         result["label"] = item.label_name
+                        result["description"] = item.description
                     else:
                         embedding_model = EmbeddingModel(
                             model_name="doubao-embedding-large-text-250515",
@@ -429,6 +430,7 @@ class MessageProcessorText:
                             result["target"] = items[0]["item_id"]
                             result["name"] = items[0]["item_name"]
                             result["label"] = items[0]["label_name"]
+                            result["description"] = items[0]["description"]
                             result["func"] = result["func"] if result["func"] in ["sit", "stand"] else "stand"
                         else:
                             items = SceneItemEntryManager().search_items_by_keywords(current_scene_id, result["target"], top_k=1)
@@ -437,6 +439,7 @@ class MessageProcessorText:
                                 result["target"] = items[0]["item_id"]
                                 result["name"] = items[0]["item_name"]
                                 result["label"] = items[0]["label_name"]
+                                result["description"] = items[0]["description"]
                                 result["func"] = result["func"] if result["func"] in ["sit", "stand"] else "stand"
                     # else:
                     #     item = SceneItemEntryManager().get_scene_item_by_id(current_scene_id, result["target"])
